00cf99b76a1deef10bb75c7d9610f68e581783bc
richiecoy <57723863+richiecoy@users.noreply.github.com>
2025-12-16 13:57:38 -0500

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,12 @@
 from app.models.connection import Connection
-from app.models.library_config import EmbyUser, EmbyLibrary, LibraryUserMapping
+from app.models.library_config import EmbyUser, EmbyLibrary, EmbyLibraryFolder, LibraryUserMapping
 from app.models.process_log import ProcessLog, ProcessRun
 
 __all__ = [
     "Connection",
     "EmbyUser",
     "EmbyLibrary",
+    "EmbyLibraryFolder",
     "LibraryUserMapping",
     "ProcessLog",
     "ProcessRun"
