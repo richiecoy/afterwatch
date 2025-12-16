b73019147b9d0e99145ce7cfe697fb5e26d9afe8
richiecoy <57723863+richiecoy@users.noreply.github.com>
2025-12-16 16:18:16 -0500

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,6 @@
 from app.routers.config import router as config_router
 from app.routers.logs import router as logs_router
 from app.routers.api import router as api_router
+from app.routers.schedule import router as schedule_router
 
-__all__ = ["config_router", "logs_router", "api_router"]
+__all__ = ["config_router", "logs_router", "api_router", "schedule_router"]
